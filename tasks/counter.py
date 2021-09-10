ae839b896880c5336678be760f6c1496c4a98375
Lasske <storm0071@ya.ru>
2021-09-10 23:09:44 +0300

    counter.py done!

--- tasks/counter.py
+++ tasks/counter.py
@@ -17,3 +17,36 @@
 - метод __iter__
 - метод __next__
 """
+
+
+class Counter:
+    value: int
+
+    def __init__(self, value=0):
+        self.value = value
+
+    def increase(self, value=1):
+        self.value += value
+
+    def decrease(self, value=1):
+        self.value -= value
+
+    def iter(self):
+        return self
+
+    def __next__(self):
+        value = self.value
+        self.value += 1
+        return value
+
+
+counter = Counter(0)
+print(counter.value)
+counter.increase(20)
+print(counter.value)
+counter.decrease(10)
+print(counter.value)
+counter.__next__()
+print(next(counter))
+
+
